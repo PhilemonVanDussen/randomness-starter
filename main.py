d82420ac5e13840b5fb3908ec0f7005f611395c5
PhilemonVanDussen <vandussenph709@northwested.org>
2024-10-22 14:30:25 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# PJ VanDussen
+# 10/22/24
+# Randomness Starter
+# 1
+import random
+colors = ['red', 'green', 'blue', 'yellow', 'purple']
+length_colors = len(colors)
+rand_index_num = random.randrange(0,length_colors)
+print(f'your random index number is: {rand_index_num}')
+print(f'your random color is: {colors[rand_index_num]}' )
+# 2
+animals = ['cheetah', 'girafe', 'elaphant', 'dolphin', 'whale', 'bird']
+length_animal = len(animals)
+rand_index_num2 = random.randrange(0,length_animal)
+print(f'your index number is: {rand_index_num2}')
+print(f'your animal is: {animals[rand_index_num2]}')
+print(f'your animal is: {animals[rand_index_num2]}')
+print(f'your animal is: {animals[rand_index_num2]}')
+# 3
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+length_nums = len(nums)
+rand_index_num3 = random.randrange(0, length_nums)
